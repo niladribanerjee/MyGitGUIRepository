3fd72281fc580eeb5098686ecae42af97fd65824
Niladri Banerjee <banerjee.niladri@gmail.com>
2019-06-09 20:51:23 +0530

    Modified src/hello.java and deleted file1.txt

--- src/hello.java
+++ src/hello.java
@@ -1,4 +1,6 @@
 public static void main (String args[])
 {
-	System.out.println ('This is my first java program');
+	System.out.println ('This is my second java program');
 }
+
+System.out.println ("Sample Change")
